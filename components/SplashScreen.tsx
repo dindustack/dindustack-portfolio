ec1d0c0ff725e7c3b2b6d01c82909ad0d1f93d37
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-01 15:38:08 +0100

    animation font-size and splash scrren fix

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,4 +1,4 @@
-import  gsap, { Sine } from "gsap";
+import gsap, { Sine } from "gsap";
 import React, { useEffect, useRef } from "react";
 
 export default function SplashScreen() {
@@ -10,7 +10,7 @@ export default function SplashScreen() {
   let logoFive: any = useRef(null);
   let logoSix: any = useRef(null);
 
-  const timeline = gsap.timeline()
+  const timeline = gsap.timeline();
 
   useEffect(() => {
     timeline.from(logoOne, {
@@ -18,47 +18,67 @@ export default function SplashScreen() {
       duration: 0.5,
       skewX: 10,
       y: -200,
-      ease: Sine.easeInOut
+      ease: Sine.easeInOut,
     });
-    timeline.from(logoTwo, {
-      opacity: 0,
-      duration: 0.5,
-      skewX: 10,
-      x: 200,
-      ease: Sine.easeInOut
-    }, "-=.2");
+    timeline.from(
+      logoTwo,
+      {
+        opacity: 0,
+        duration: 0.5,
+        skewX: 10,
+        x: 200,
+        ease: Sine.easeInOut,
+      },
+      "-=.2"
+    );
 
-    timeline.from(logoThree, {
-      opacity: 0,
-      duration: 0.5,
-      skewX: 10,
-      y: 200,
-      ease: Sine.easeInOut,
-    }, "-=.2");
+    timeline.from(
+      logoThree,
+      {
+        opacity: 0,
+        duration: 0.5,
+        skewX: 10,
+        y: 200,
+        ease: Sine.easeInOut,
+      },
+      "-=.2"
+    );
 
-    timeline.from(logoFour, {
-      opacity: 0,
-      duration: 0.5,
-      skewX: 10,
-      y: -200,
-      ease: Sine.easeInOut,
-    }, "-=.2");
+    timeline.from(
+      logoFour,
+      {
+        opacity: 0,
+        duration: 0.5,
+        skewX: 10,
+        y: -200,
+        ease: Sine.easeInOut,
+      },
+      "-=.2"
+    );
 
-    timeline.from(logoFive, {
-      opacity: 0,
-      duration: 0.5,
-      skewX: 10,
-      y: -200,
-      ease: Sine.easeInOut,
-    }, "-=.2");
+    timeline.from(
+      logoFive,
+      {
+        opacity: 0,
+        duration: 0.5,
+        skewX: 10,
+        y: -200,
+        ease: Sine.easeInOut,
+      },
+      "-=.2"
+    );
 
-    timeline.from(logoSix, {
-      opacity: 0,
-      duration: 0.5,
-      skewX: 10,
-      y: 200,
-      ease: Sine.easeInOut,
-    }, "-=.2");
+    timeline.from(
+      logoSix,
+      {
+        opacity: 0,
+        duration: 0.5,
+        skewX: 10,
+        y: 200,
+        ease: Sine.easeInOut,
+      },
+      "-=.2"
+    );
 
     timeline.from(logoTwo, {
       delay: 0.1,
@@ -68,48 +88,62 @@ export default function SplashScreen() {
       y: -200,
     });
 
-    timeline.to(logoFive, {
-      duration: 0.5,
-      opacity: 0,
-      ease: Sine.easeInOut,
-      x: 300,
-    }, "-=.2");
+    timeline.to(
+      logoFive,
+      {
+        duration: 0.5,
+        opacity: 0,
+        ease: Sine.easeInOut,
+        x: 300,
+      },
+      "-=.2"
+    );
 
-    timeline.to(logoThree, {
-      duration: 0.5,
-      opacity: 0,
-      ease: Sine.easeInOut,
-      x: -300,
-    }, "-=.2");
+    timeline.to(
+      logoThree,
+      {
+        duration: 0.5,
+        opacity: 0,
+        ease: Sine.easeInOut,
+        x: -300,
+      },
+      "-=.2"
+    );
 
-    timeline.to(logoSix, {
-      duration: 0.5,
-      opacity: 0,
-      ease: Sine.easeInOut,
-      x: 300,
-    }, "-=.2");
+    timeline.to(
+      logoSix,
+      {
+        duration: 0.5,
+        opacity: 0,
+        ease: Sine.easeInOut,
+        x: 300,
+      },
+      "-=.2"
+    );
 
-    timeline.to(logoFour, {
-      duration: 0.5,
-      opacity: 0,
-      ease: Sine.easeInOut,
-      x: 300,
-    }, "-=.2");
+    timeline.to(
+      logoFour,
+      {
+        duration: 0.5,
+        opacity: 0,
+        ease: Sine.easeInOut,
+        x: 300,
+      },
+      "-=.2"
+    );
 
     timeline.to(logoOne, {
-      position: "absolute"
+      position: "absolute",
     });
 
     timeline.to(logoOne, {
       fontSize: "2vmax",
       top: "10px",
       left: "10px",
-      ease: Sine.easeInOut
+      ease: Sine.easeInOut,
     });
+  });
 
-
-
-  })
   return (
     <div className="text-[#E7E7E7] uppercase">
       <div
@@ -121,7 +155,7 @@ export default function SplashScreen() {
           <div className="flex absolute items-center justify-center w-full h-screen">
             <div
               ref={(el) => (logoOne = el)}
-              className="font-monument font-semibold text-[#E7E7E7] text-[6vmax] md:text-[10.1vmax] mix-blend-difference leading-none"
+              className="font-monument font-semibold text-[#E7E7E7] text-[3vmax] md:text-[10.1vmax] mix-blend-difference leading-none"
             >
               dindustack
             </div>
@@ -131,7 +165,7 @@ export default function SplashScreen() {
           <div className="flex absolute items-center justify-center w-full h-screen">
             <div
               ref={(el) => (logoTwo = el)}
-              className="font-monument font-semibold text-[#01FF01] text-[6.1vmax] md:text-[10.2vmax] mix-blend-difference leading-none"
+              className="font-monument font-semibold text-[#01FF01] text-[3.1vmax] md:text-[10.2vmax] mix-blend-difference leading-none"
             >
               dindustack
             </div>
@@ -141,7 +175,7 @@ export default function SplashScreen() {
           <div className="flex absolute items-center justify-center w-full h-screen">
             <div
               ref={(el) => (logoThree = el)}
-              className="font-monument font-semibold text-[#FC1F1F] text-[6.2vmax] md:text-[10.3vmax] mix-blend-difference leading-none"
+              className="font-monument font-semibold text-[#FC1F1F] text-[3.2vmax] md:text-[10.3vmax] mix-blend-difference leading-none"
             >
               dindustack
             </div>
@@ -151,7 +185,7 @@ export default function SplashScreen() {
           <div className="flex absolute items-center justify-center w-full h-screen">
             <div
               ref={(el) => (logoFour = el)}
-              className="font-monument font-semibold text-[#8CF7F7] text-[6.3vmax] md:text-[10.4vmax] mix-blend-difference leading-none"
+              className="font-monument font-semibold text-[#8CF7F7] text-[3.3vmax] md:text-[10.4vmax] mix-blend-difference leading-none"
             >
               dindustack
             </div>
@@ -161,7 +195,7 @@ export default function SplashScreen() {
           <div className="flex absolute items-center justify-center w-full h-screen">
             <div
               ref={(el) => (logoFive = el)}
-              className="font-monument font-semibold text-[#4254F8] text-[6.4vmax] md:text-[10.5vmax] mix-blend-difference leading-none"
+              className="font-monument font-semibold text-[#4254F8] text-[3.4vmax] md:text-[10.5vmax] mix-blend-difference leading-none"
             >
               dindustack
             </div>
@@ -171,7 +205,7 @@ export default function SplashScreen() {
           <div className="flex absolute items-center justify-center w-full h-screen">
             <div
               ref={(el) => (logoSix = el)}
-              className="font-monument font-semibold text-[#AC00AC] text-[6.5vmax] md:text-[10.6vmax] mix-blend-difference leading-none"
+              className="font-monument font-semibold text-[#AC00AC] text-[3.5vmax] md:text-[10.6vmax] mix-blend-difference leading-none"
             >
               dindustack
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
-import SplashScreen from '@/components/SplashScreen'
-import Head from 'next/head'
-
-
+import React, { useState, useEffect } from "react";
+import SplashScreen from "@/components/SplashScreen";
+import Head from "next/head";
 
 export default function HomePage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  /**
+   * Display Splash Screen
+   */
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+    return () => clearTimeout(timeout)
+  }, []);
+
+  
+
   return (
     <>
       <Head>
         <title>DinduStack</title>
-        <meta name="description" content="Portfolio showcasing frontend experience" />
+        <meta
+          name="description"
+          content="Portfolio showcasing frontend experience"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SplashScreen />
+
+      {isLoading ? <SplashScreen /> : <div>Hello</div>}
     </>
-  )
+  );
 }
