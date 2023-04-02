1140ddfe0001e6c12c51d486b46f1b6e9962410d
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-02 19:56:16 +0100

    favicon setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import SplashScreen from "@/components/SplashScreen";
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function HomePage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -26,6 +27,24 @@ export default function HomePage() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
 
       {isLoading ? (
@@ -33,6 +52,7 @@ export default function HomePage() {
       ) : (
         <div className="text-[#e7e7e7] uppercase">
           <Navbar />
+          <Hero />
         </div>
       )}
     </>
