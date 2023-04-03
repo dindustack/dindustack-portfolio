83e50f991e06d1b1f3b5c6c9afa7db649fbceb77
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-03 17:05:06 +0100

    custom cursor fix

--- components/CustomCursor.tsx
+++ components/CustomCursor.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function CustomCursor() {
+  const cursorRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (event) => {
+      const {clientX, clientY} = event
+      const mouseX = clientX - cursorRef.current.clientWidth / 2
+      const mouseY = clientY - cursorRef.current.clientHeight / 2
+
+      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`
+    })
+  })
   return (
-    <div className="absolute z-[1000] rounded-[50%] w-[5rem] h-[5rem] border border-white pointer-events-none overflow-hidden translate-z-0"></div>
+    <div ref={cursorRef} className="absolute z-[1000] rounded-[50%] w-[5rem] h-[5rem] border border-white pointer-events-none overflow-hidden translate-z-0"></div>
   );
 }
