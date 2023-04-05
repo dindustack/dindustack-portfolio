551bd0074b772c8036b3f7853cd5d8bc80e51200
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-06 00:54:11 +0100

    mobile and tablet responsive fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,35 +6,60 @@ import slideImageTwo from "../images/Chinwendu-Agbaetuo-5.jpeg";
 
 export default function Hero() {
   return (
-    <div className="pt-[13rem] md:pt-[15rem] pb-[7.5rem] w-[100vw] h-[95vh] overflow-hidden">
-      <div className="py-0 px-[1.3rem] md:px-[2.5rem] md:max-w-[90rem] mx-auto my-0">
-        <div className="flex flex-col items-center md:items-stretch md:grid md:grid-cols-3 md:gap-4 ">
-          <h1 className="text-4xl font-monument font-normal md:text-4xl text-center md:text-left lg:text-7xl -mb-[0.8rem] md:mb-0 leading-none z-20">
-            Hey, i&apos;m Chinwendu
+    <div className="pt-[10rem] lg:pt-[15rem] pb-[7.5rem] px-[1.5rem] md:px-[3.375rem] lg:px-[7rem] w-[100vw] h-[95vh] overflow-hidden">
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-between">
+        <div className="relative flex justify-start md:justify-center flex-col md:pb-0 lg:pb-[6rem] pt-[2.125rem] md:pt-[0] md:gap-y-[1rem] lg:gap-y-[4rem] w-[100%] lg:w-[18.5rem] xl:w-[17.7rem]">
+          <h1 className=" mb-[10px] text-3xl font-monument font-normal md:text-4xl text-center md:text-left lg:text-7xl leading-none z-20">
+            <span className="leading-none">Hey</span>
+            <br />
+            <span>i&apos;m</span>
+            <span className="ml-2 md:ml-8">Chinwendu</span>
           </h1>
-          <div className="relative">
-            <Image
-              alt="Chinwendu Agbaetuo"
-              src={slideImageOne}
-              className="w-[19rem] h-[19rem] md:w-[29rem] md:h-[29rem] max-w-[100%] border-white  border-2 rounded-[50%] object-cover"
-            />
-
-            <Image
-              alt="Chinwendu Agbaetuo"
-              src={slideImageTwo}
-              className="absolute top-0 opacity-0 left-0 w-[19rem] h-[19rem] md:w-[29rem] md:h-[29rem] border-white border-2 max-w-[100%] rounded-[50%] object-cover transition-all duration-700 ease-in-out hover:opacity-100"
-            />
+          <div className="hidden md:flex flex-col items-start gap-y-[1rem]">
+            <a href="https://www.instagram.com/dindustack/">
+              <h5 className="font-neutralFace font-normal text-base leading-none hover:underline hover:decoration-2 hover:underline-offset-4">
+                instagram
+              </h5>
+            </a>
+            <a href="https://www.linkedin.com/in/dindustack/">
+              <h5 className="font-neutralFace font-normal text-base leading-none hover:underline hover:decoration-2 hover:underline-offset-4">
+                linkedin
+              </h5>
+            </a>
+            <a href="https://twitter.com/dindustack">
+              <h5 className="font-neutralFace font-normal text-base leading-none hover:underline hover:decoration-2 hover:underline-offset-4">
+                twitter
+              </h5>
+            </a>
+            <a href="https://dribbble.com/dindustack">
+              <h5 className="font-neutralFace font-normal text-base leading-none hover:underline hover:decoration-2 hover:underline-offset-4">
+                dribbble
+              </h5>
+            </a>
           </div>
+        </div>
+        <div className="relative flex flex-col justify-center items-center">
+          <Image
+            alt="Chinwendu Agbaetuo"
+            src={slideImageOne}
+            className="w-[19rem] h-[19rem] md:w-[22rem] md:h-[22rem] lg:w-[29rem] lg:h-[29rem] max-w-[100%] border-white  border-2 rounded-[50%] object-cover"
+          />
 
-          <div className="flex justify-end flex-col">
-            <h2 className="text-3xl font-monument font-light md:text-4xl text-center md:text-right lg:text-5xl -mt-[0.5rem] md:mt-0 leading-none z-[2]">
-              creative
-              <br /> 
-              <span className="relative lg:-left-[12rem]">frontend</span>
-              <br />
-               <span>developer</span>
-            </h2>
-          </div>
+          <Image
+            alt="Chinwendu Agbaetuo"
+            src={slideImageTwo}
+            className="absolute top-0 opacity-0 left-0 w-[19rem] h-[19rem] md:w-[22rem] md:h-[22rem] lg:w-[29rem] lg:h-[29rem] border-white border-2 max-w-[100%] rounded-[50%] object-cover transition-all duration-700 ease-in-out hover:opacity-100"
+          />
+        </div>
+
+        <div className="relative flex flex-col gap-y-[2rem] md:justify-center md:items-end pt-0 -mt-2 lg:pt-[15rem] w-[100%] lg:w-[18.5rem] xl:w-[17.7rem]">
+          <h2 className="text-3xl font-monument font-light md:text-4xl text-center md:text-right lg:text-6xl leading-none z-[2]">
+            creative
+            <br />
+            <span className="relative lg:-left-[16rem]">frontend</span>
+            <br />
+            <span>developer</span>
+          </h2>
         </div>
       </div>
     </div>
