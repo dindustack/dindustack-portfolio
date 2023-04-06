677359e2cf29fc0989dc28377a7d05662b7f9690
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-06 22:50:33 +0100

    styling marquee fix

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -3,40 +3,41 @@ import Marquee from "react-fast-marquee";
 
 export default function MarqueeSection() {
   return (
-    <div className="pb-8">
+    <div className="pt-8 pb-8">
       <Marquee
         gradient={false}
-        speed={25}
+        speed={100}
+        pauseOnHover={true}
         className="relative flex w-full border-white border-t border-b"
       >
-        <div className="py-12">
-          <span className="text-base mx-4">Frontend engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">ui engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">designer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">Frontend engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">ui engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">designer</span>
-          <span className="text-base mx-4">•</span>
+        <div className="py-8">
+          <span className="text-base leading-none mx-4">Frontend engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">ui engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">designer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">Frontend engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">ui engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">designer</span>
+          <span className="text-base leading-none mx-4">•</span>
         </div>
 
-        <div className="py-12">
-          <span className="text-base mx-4">Frontend engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">ui engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">designer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">Frontend engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">ui engineer</span>
-          <span className="text-base mx-4">•</span>
-          <span className="text-base mx-4">designer</span>
-          <span className="text-base mx-4">•</span>
+        <div className="py-8">
+          <span className="text-base leading-none mx-4">Frontend engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">ui engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">designer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">Frontend engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">ui engineer</span>
+          <span className="text-base leading-none mx-4">•</span>
+          <span className="text-base leading-none mx-4">designer</span>
+          <span className="text-base leading-none mx-4">•</span>
         </div>
       </Marquee>
     </div>
