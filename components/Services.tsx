3862f9f79a767d021bca8ca746ded6db92c12aa2
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-08 18:45:29 +0100

    remove mx-auto from services

--- components/Services.tsx
+++ components/Services.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Services() {
   return (
     <div className='relative py-[6.25rem] px-0'>
-        <div className="max-w-[90rem] px-[0.75rem] md:px-[2.5rem] mx-auto">
+        <div className="max-w-[90rem] px-[0.75rem] md:px-[2.5rem]">
             <div className='pb-[3.75rem]'>
                 <h2 className='font-monument normal-case text-[2rem] md:text-[3.58rem] leading-[1.1] -tracking-[0.02rem]'>I can help you with ...</h2>
             </div>
