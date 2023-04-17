e40f40458a5497f79e299ef49a5cad54e73c99ad
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-17 20:29:07 +0100

    text animation fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,12 +8,13 @@ import slideImageTwo from "../images/Chinwendu_Agbaetuo-2.jpeg";
 export default function Hero() {
   const heroAnimation = gsap.timeline();
   const socialAnimation = useRef(null);
+  const textAnimation = useRef(null);
 
   useEffect(() => {    
     heroAnimation.fromTo(
       socialAnimation.current,
       {
-        y: 0,
+        y: 50,
         opacity: 0,
       },
       {
@@ -21,15 +22,30 @@ export default function Hero() {
         opacity: 1,
         duration: 1,
         ease: Power2.easeInOut,
+        stagger: {
+          amount: 0.4,
+        },
       }
     );
-  }, [socialAnimation, heroAnimation]);
+
+    heroAnimation.fromTo(textAnimation.current, {
+      x: 500,
+      opacity: 0,
+    },
+    {
+      x: 0,
+      opacity: 1,
+      duration: 1,
+      ease: Power2.easeInOut,
+    }
+    );
+  }, [heroAnimation]);
 
   return (
     <div className="pt-[10rem] lg:pt-[15rem] pb-[7.5rem] px-[2.5rem] mx-auto w-[100vw] h-[95vh] overflow-hidden">
       <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-between">
         <div className="relative flex justify-start md:justify-center flex-col md:pb-0 lg:pb-[6rem] pt-[2.125rem] md:pt-[0] md:gap-y-[1rem] lg:gap-y-[4rem] w-[100%] lg:w-[18.5rem] xl:w-[17.7rem]">
-          <h1 className=" mb-[10px] text-3xl font-monument font-bold md:text-4xl text-center md:text-left xl:text-[4rem] z-[2]">
+          <h1 className="mb-[10px] text-3xl font-monument font-bold md:text-4xl text-center md:text-left xl:text-[4rem] z-[2]">
             <span className="leading-[1.5]">Hey</span>
             <br />
             <span>i&apos;m</span>
@@ -85,7 +101,7 @@ export default function Hero() {
             <br />
             <span>developer</span>
           </h2>
-          <div className="break-words font-light normal-case text-base hidden md:flex md:w-[18rem]">
+          <div ref={textAnimation} className="break-words font-light normal-case text-base hidden md:flex md:w-[18rem]">
             Crafting beautiful and intuitive user experiences, one line of code
             at a time.
           </div>
