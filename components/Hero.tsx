fd6d453f05be012a23d073ed135ce99a355ee27e
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-04-17 22:55:56 +0100

    content animation fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,8 +9,10 @@ export default function Hero() {
   const heroAnimation = gsap.timeline();
   const socialAnimation = useRef(null);
   const textAnimation = useRef(null);
+  const nameAnimation = useRef(null);
+  const roleAnimation = useRef(null);
 
-  useEffect(() => {    
+  useEffect(() => {
     heroAnimation.fromTo(
       socialAnimation.current,
       {
@@ -28,16 +30,49 @@ export default function Hero() {
       }
     );
 
-    heroAnimation.fromTo(textAnimation.current, {
-      x: 500,
-      opacity: 0,
-    },
-    {
-      x: 0,
-      opacity: 1,
-      duration: 1,
-      ease: Power2.easeInOut,
-    }
+    heroAnimation.fromTo(
+      textAnimation.current,
+      {
+        x: 500,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: Power2.easeInOut,
+      }
+    );
+
+    heroAnimation.fromTo(
+      nameAnimation.current,
+      {
+        x: -500,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: Power2.easeInOut,
+      }
+    );
+
+    heroAnimation.fromTo(
+      roleAnimation.current,
+      {
+        y: -50,
+        opacity: 0,
+      },
+      {
+        y: -10,
+        opacity: 1,
+        duration: 1,
+        ease: Power2.easeInOut,
+        stagger: {
+          amount: 0.4,
+        },
+      }
     );
   }, [heroAnimation]);
 
@@ -45,7 +80,10 @@ export default function Hero() {
     <div className="pt-[10rem] lg:pt-[15rem] pb-[7.5rem] px-[2.5rem] mx-auto w-[100vw] h-[95vh] overflow-hidden">
       <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-between">
         <div className="relative flex justify-start md:justify-center flex-col md:pb-0 lg:pb-[6rem] pt-[2.125rem] md:pt-[0] md:gap-y-[1rem] lg:gap-y-[4rem] w-[100%] lg:w-[18.5rem] xl:w-[17.7rem]">
-          <h1 className="mb-[10px] text-3xl font-monument font-bold md:text-4xl text-center md:text-left xl:text-[4rem] z-[2]">
+          <h1
+            ref={nameAnimation}
+            className="mb-[10px] text-3xl font-monument font-bold md:text-4xl text-center md:text-left xl:text-[4rem] z-[2]"
+          >
             <span className="leading-[1.5]">Hey</span>
             <br />
             <span>i&apos;m</span>
@@ -92,7 +130,10 @@ export default function Hero() {
         </div>
 
         <div className="relative flex flex-col gap-y-[2rem] md:justify-center md:items-end pt-0 -mt-2 lg:pt-[15rem] w-[100%] lg:w-[18.5rem] xl:w-[17.7rem]">
-          <h2 className="text-3xl font-monument font-light md:text-4xl text-center md:text-right xl:text-6xl leading-none z-[2]">
+          <h2
+            ref={roleAnimation}
+            className="text-3xl font-monument font-light md:text-4xl text-center md:text-right xl:text-6xl leading-none z-[2]"
+          >
             creative
             <br />
             <span className="relative md:-left-[8rem] xl:-left-[16rem]">
@@ -101,7 +142,10 @@ export default function Hero() {
             <br />
             <span>developer</span>
           </h2>
-          <div ref={textAnimation} className="break-words font-light normal-case text-base hidden md:flex md:w-[18rem]">
+          <div
+            ref={textAnimation}
+            className="break-words font-light normal-case text-base hidden md:flex md:w-[18rem]"
+          >
             Crafting beautiful and intuitive user experiences, one line of code
             at a time.
           </div>
