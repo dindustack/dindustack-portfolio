275f77a018efa00ccd515797b920143293b4c348
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-06-09 21:45:02 +0100

    projects section styling fix

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from "react";
-import { Element } from "react-scroll";
 
 import ProjectItem from "./Item";
 import { projects } from "@/constants/projects";
@@ -24,7 +23,7 @@ export function Projects() {
 
   return (
     <Sidebar>
-      <div className="flex flex-col justify-center min-h-screen ">
+      <div className="flex flex-col justify-center min-h-screen">
         <div className="font-migra-extrabold text-[3.5rem] text-gray-900 uppercase my-12">
           works
           <span className="ml-4 normal-case text-[1.5rem]">
@@ -55,65 +54,22 @@ export function Projects() {
           ref={projectBoxRef}
           offsets={offsets.current}
         />
+
+        <div className="flex flex-col justify-center items-center gap-2 mt-[13.5rem] mb-[8.5rem]">
+          <div className="text-[2.5rem] text-gray-900 font-migra-extrabold uppercase leading-[auto] mb-2">
+            Want to hire me?
+          </div>
+          <div className="normal-case font-eastman text-[1.5rem]">
+            <span>Reach out via</span>
+            <a
+              href="mailto:contact@dindustack.com"
+              className="relative py-1 ml-1"
+            >
+              contact@dindustack.com
+            </a>
+          </div>
+        </div>
       </div>
     </Sidebar>
-    // <Element name="projects">
-    //   <div className="relative py-[6.25rem] font-monument">
-    //     <div className="px-[0.75rem] md:px-[2.5rem] mx-auto">
-    //       <div className="pb-[1.25rem]">
-    //         <div className="flex space-x-4 items-start">
-    //           <h3 className="text-[1.5rem] md:text-[2.4rem] normal-case leading-none -tracking-[0.01rem]">
-    //             Selected Work
-    //           </h3>
-    //           <span className="normal-case">(Work in progress)</span>
-    //         </div>
-    //       </div>
-
-    //       <div
-    //         onMouseEnter={() =>
-    //           projectBoxRef.current?.animateIn()
-    //         }
-    //         onMouseLeave={() =>
-    //           projectBoxRef.current?.animateOut()
-    //         }
-    //       >
-    //         {React.Children.toArray(
-    //           projects.map((project, index) => (
-    //             <ProjectItem
-    //               serialNumber={project.id}
-    //               projectName={project.projectName}
-    //               index={index}
-    //               onEnter={(index) => setActiveIndex(index)}
-    //               onMove={(event) => handleMove(event)}
-    //             />
-    //           ))
-    //         )}
-    //       </div>
-
-    //       <ProjectBox
-    //         projectImages={projects}
-    //         activeIndex={activeIndex}
-    //         ref={projectBoxRef}
-    //         offsets={offsets.current}
-    //       />
-
-    //       {/* Project Footer */}
-    //       <div className="pt-[3rem]">
-    //         <h3 className="text-[2.4rem] font-monument normal-case leading-none -tracking-[0.01rem] mb-3">
-    //           Want to see more?
-    //         </h3>
-    //         <p className="normal-case font-migra text-xl">
-    //           <span>Reach out to me at</span>
-    //           <a
-    //             href="mailto:contact@dindustack.com"
-    //             className="relative py-1 ml-1"
-    //           >
-    //             contact@dindustack.com
-    //           </a>
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Element>
   );
 }
