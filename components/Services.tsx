9ea0518ade6d16bad1508c2abbebe53e2c2560a2
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-07-04 09:54:22 +0100

    services text fix

--- components/Services.tsx
+++ components/Services.tsx
@@ -17,12 +17,12 @@ export function Services() {
         <div className="lg:pl-[7.5rem] grid md:grid-cols-2 gap-x-32 gap-y-16">
           {React.Children.toArray(
             services.map((service) => (
-              <div className="flex flex-row items-start p-0 gap-2">
-                <span className=" font-migra-extrabold font-extrabold text-gray-300 text-[4rem] leading-none">
+              <div className="flex flex-col md:flex-row items-start p-0 gap-2">
+                <span className=" font-migra-extrabold font-extrabold text-gray-300 text-[2.5rem] md:text-[4rem] leading-none mb-2">
                   {service.id}
                 </span>
                 <div className="flex flex-col items-start p-0 gap-2 max-w-[21.875rem]">
-                  <span className="font-eastman-medium text-gray-900 text-[2.5rem] leading-[2.5rem] mb-4">
+                  <span className="font-eastman-medium text-gray-900 text-[1.75rem] md:text-[2.5rem] leading-[2rem] md:leading-[2.5rem] lg:mb-4">
                     {service.service}
                   </span>
                   <span className="text-[1.125rem] font-eastman text-gray-500 normal-case">
