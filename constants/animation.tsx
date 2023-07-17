dd19674eb540a198de7312ebe28db1efb2ba7528
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-07-17 16:59:31 +0100

    partial fix for projects re-rendering

--- constants/animation.tsx
+++ constants/animation.tsx
@@ -1,26 +1,34 @@
 import { useRef, useEffect, useCallback } from "react";
 
 export function useRafFn(callback: () => void): () => void {
-  const callbackRef = useRef<() => void>(callback);
+	const callbackRef = useRef<() => void>(callback);
 
-  useEffect(() => {
-    callbackRef.current = callback;
-  }, [callback]);
+	useEffect(() => {
+		callbackRef.current = callback;
+	}, [callback]);
 
-  const frameRef = useRef<number | undefined>();
+	const frameRef = useRef<number | undefined>();
 
-  const rafFn = useCallback(() => {
-    const callback = callbackRef.current;
-    if (callback) {
-      callback();
-      frameRef.current = requestAnimationFrame(rafFn);
-    }
-  }, []);
+	// const rafFn = useCallback(() => {
+	// 	const callback = callbackRef.current;
+	// 	if (callback) {
+	// 		callback();
+	// 		frameRef.current = requestAnimationFrame(rafFn);
+	// 	}
+	// }, []);
 
-  useEffect(() => {
-    frameRef.current = requestAnimationFrame(rafFn);
-    return () => cancelAnimationFrame(frameRef.current as number);
-  }, [rafFn]);
+	const rafFn = useCallback(() => {
+		const callback = callbackRef.current;
+		if (callback) {
+			callback();
+			frameRef.current = requestAnimationFrame(rafFn);
+		}
+	}, []) as () => void;
 
-  return rafFn;
+	useEffect(() => {
+		frameRef.current = requestAnimationFrame(rafFn);
+		return () => cancelAnimationFrame(frameRef.current as number);
+	}, [rafFn]);
+
+	return rafFn;
 }
