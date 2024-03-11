8c0d3aa01f4a53c1574311c61c565ddc73ad9b1e
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2024-03-11 21:16:45 +0100

    style: fix hero section styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { gsap, Power2 } from "gsap";
 import hoverEffect from "hover-effect";
-import React, { forwardRef, useEffect, useRef } from "react";
-import { Sidebar } from "./Sidebar";
+import { forwardRef, useEffect, useRef } from "react";
 
 type HeroProps = {
   id: string;
@@ -86,13 +85,16 @@ export const Hero = forwardRef<HTMLDivElement, HeroProps>(function Hero(
   });
 
   return (
-    <section
-      className="flex flex-col-reverse items-center justify-center relative h-screen"
-      id={id}
-      ref={ref}
-    >
+    <section className="relative  h-screen" id={id} ref={ref}>
+      <div
+        className="absolute top-1/2 lg:top-[30%] left-1/2 
+      -translate-x-1/2 -translate-y-1/2"
+      >
+        <div className="hero-image"></div>
+      </div>
+
       {/* absolute text to the left */}
-      <div className="lg:absolute lg:left-0 z-[20]">
+      <div className="lg:absolute top-[40%] lg:left-0 z-[20]">
         <div
           ref={heroTextAnimation}
           className="leading-[3.75rem] text-gray-900"
@@ -118,7 +120,6 @@ export const Hero = forwardRef<HTMLDivElement, HeroProps>(function Hero(
           </div>
         </div>
       </div>
-      <div className="hero-image"></div>
     </section>
   );
 });
